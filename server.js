2a0868f2158c71abb1f43d4fc9c838c0e3bbce50
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2021-10-30 21:58:26 +0300

    second try

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
 
-app.listen(PORT)
+app.listen(PORT, () => console.log(`server started on ${PORT}`))
